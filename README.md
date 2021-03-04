24cc1cc6183a985eafb2fdb80b3c1becb85838a2
Riccardo Giorato <giorat.96@gmail.com>
2021-03-04 19:36:13 +0100

    add better example and guide

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # node-sheets-demo
+
+## Install Dependencies
+
+```javascript
+yarn add node-sheets dotenv
+```
+
+## Create Google API Key
+
+You can generate your own API Key here:
+
+1. Register here with your Google account and create a new project ["Google Developer Console"](https://console.developers.google.com/)
+
+![](img/create.png)
+
+2. Then go in the "Credentials" tab create a new API Keys.
+
+![](img/credentials.png)
+
+## Add the API Key to ENV
+
+Remember to create a ".env" file with this content:
+
+```
+GOOGLE_SHEET_KEY=HERE_YOUR_API_KEY
+```
+
+## Run the script
+
+To run this script run these commands
+
+```javascript
+yarn install
+yarn dev
+```
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ const Sheets = require("node-sheets").default;
 const dotenv = require("dotenv");
 dotenv.config();
 
+/*
+Google Sheet example with a list of students here
+https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit#gid=0
+*/
+
 (async () => {
   try {
-    const gs = new Sheets("1amfst1WVcQDntGe6walYt-4O5SCrHBD5WntbjhvfIm4");
+    const gs = new Sheets("1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms");
     await gs.authorizeApiKey(process.env.GOOGLE_SHEET_KEY);
-    const table = await gs.tables("Class Data!A1:H3");
+    const table = await gs.tables("Class Data!A:F");
     console.log(table.headers);
+    console.log();
     console.log(table.rows);
   } catch (err) {
     console.error(err);
